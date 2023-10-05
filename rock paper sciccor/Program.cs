ad082bceb89c487e41a0d14c1b946e6265742c8c
mads04144 <dsadfgttf@gmail.com>
2023-10-05 14:15:09 +0200

    Add project files.

---
+++ rock paper sciccor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rock_paper_sciccor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int PlayerScore = 0;
+            int Enemyscore = 0;
+
+            Console.WriteLine("Sten Saks Papir!");
+
+            while(PlayerScore != 3 && Enemyscore != 0)
+            {
+                Console.WriteLine("Player score - " + PlayerScore + ". Enemy Score - " + Enemyscore);
+
+            }
+
+
+
+        }
+    }
+}
